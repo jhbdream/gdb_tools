672f9772e57001a466b1727ee5000f3f2642ba63
jihongbin <longma@orbbec.com>
2022-07-19 20:54:39 +0800

    update
    Signed-off-by: jihongbin <longma@orbbec.com>

--- page_dump/mmu.py
+++ page_dump/mmu.py
@@ -25,6 +25,12 @@ def extract_no_shift(value, s, e):
     mask = ((1<<(e + 1))-1) & ~((1<<s) - 1)
     return (value & mask)
 
+SV_MODE = {
+    0: "OFF",
+    8: "SV39",
+    9: "SV48",
+}
+
 class Page(gdb.Command):
     def __init__(self):
         super(Page, self).__init__("page", gdb.COMMAND_SUPPORT, gdb.COMPLETE_NONE, True)
@@ -32,17 +38,14 @@ class Page(gdb.Command):
 
     def invoke(self, arg, from_tty):
         print("start exec page command...")
+
         satp = read_reg("satp")
-        print("satp: [0x%x]" % satp)
         
-        ppn = extract(satp, 0, 43)
-        addr = ppn << 12
-        asid = extract(satp, 44, 59)
+        addr = extract(satp, 0, 43) << 12
         mode = extract(satp, 60, 63)
 
-        print("ppn: [0x%016x]" % ppn)
-        print("mode: [0x%d]" % mode)
-        print("addr: [0x%x]" % addr)
+        print("MMU Mode:" + SV_MODE.get(mode, "unknown"))
+        print("pgdir addr: [0x%x]" % addr)
 
         print("end exec page command!")
 
